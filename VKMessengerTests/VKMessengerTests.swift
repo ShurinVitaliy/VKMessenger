c6ae3ea0cdb69f548534b630570402e88869c3aa
Vitaliy <34574399+ShurinVitaliy@users.noreply.github.com>
2019-01-02 17:34:52 +0300

    Initial Commit

---
+++ VKMessengerTests/VKMessengerTests.swift
@@ -0,0 +1,34 @@
+//
+//  VKMessengerTests.swift
+//  VKMessengerTests
+//
+//  Created by Vitaly Shurin on 1/2/19.
+//  Copyright © 2019 Vitaly Shurin. All rights reserved.
+//
+
+import XCTest
+@testable import VKMessenger
+
+class VKMessengerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
